a69cf8b72440f6f8900c5620086d47f9a48023c2
jol79 <jolhuck@gmail.com>
2022-01-17 18:04:32 +0200

    Close #3

--- welcomer/welcomes/urls.py
+++ welcomer/welcomes/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main, name='main')
+    path('', views.main, name='main'),
+    path('users', views.users, name='users'),
+    path('createNewUser', views.createNewUser, name='createNewUser'),
 ]
